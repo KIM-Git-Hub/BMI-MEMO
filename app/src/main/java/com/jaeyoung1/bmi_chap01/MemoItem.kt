45e994f9f1fc76225be345e4e81da23c45bac28f
deliciousBanana <100995721+deliciousBanana@users.noreply.github.com>
2022-03-22 23:48:41 +0900

    test

--- app/src/main/java/com/jaeyoung1/bmi_chap01/MemoItem.kt
+++
@@ -1,4 +0,0 @@
-package com.jaeyoung1.bmi_chap01
-
-class MemoItem(val time : String, val weight : String, val bmiNum : String, val bmiString : String) {
-}
--- app/src/main/java/com/jaeyoung1/bmi_chap01/ResultF.kt
+++ app/src/main/java/com/jaeyoung1/bmi_chap01/ResultF.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.jaeyoung1.bmi_chap01.BusEvent.MemoF1toResultFEventBusItem
@@ -22,16 +23,13 @@ class ResultF : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
-        val view = inflater.inflate(R.layout.activity_result, container, false)
+        val view = inflater.inflate(R.layout.result_f, container, false)
 
         EventBus.getDefault().register(this)
 
-
-
-        val bmi: Double = weightValue / ((heightValue / 100.0) * 2.0)
+        val bmi: Double = weightValue / ((heightValue * 0.01) * (heightValue * 0.01))
 
         val resultText = when {
-
             bmi >= 30 -> "고도비만"
             bmi >= 25 -> "비만"
             bmi >= 23 -> "과체중"
@@ -39,19 +37,28 @@ class ResultF : Fragment() {
             else -> "저체중"
         }
 
+        val bmi1 = view.findViewById<ImageView>(R.id.bmi_1)
+        val bmi2 = view.findViewById<ImageView>(R.id.bmi_2)
+        val bmi3 = view.findViewById<ImageView>(R.id.bmi_3)
+        val bmi4 = view.findViewById<ImageView>(R.id.bmi_4)
+        val bmi5 = view.findViewById<ImageView>(R.id.bmi_5)
+        when(resultText){
+            "고도비만" -> bmi5.visibility = View.VISIBLE
+            "비만" -> bmi4.visibility = View.VISIBLE
+            "과체중" -> bmi3.visibility = View.VISIBLE
+            "정상체중" -> bmi2.visibility = View.VISIBLE
+            "저체중" -> bmi1.visibility = View.VISIBLE
+        }
+
         val bmiNum = view.findViewById<TextView>(R.id.bmiNum)
         bmiNum.text = String.format("%.2f", bmi) // 소수점 버림
         val bmiNumValue = bmiNum.text.toString()
-
-        val bmiString = view.findViewById<TextView>(R.id.bmiString)
-        bmiString.text = resultText
-        val bmiStringValue = bmiString.text.toString()
 
         val addMemoButton = view.findViewById<TextView>(R.id.addMemoButton)
 
         addMemoButton.setOnClickListener {
 
-            EventBus.getDefault().postSticky(ResultF2toMemoF2BusEventItem(heightValue.toString(), bmiNumValue, bmiStringValue, 1))
+            EventBus.getDefault().postSticky(ResultF2toMemoF2BusEventItem(heightValue.toString(), bmiNumValue, resultText, 1))
             (activity as MainActivity).setFrag(2)
 
         }
